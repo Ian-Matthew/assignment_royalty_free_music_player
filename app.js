5e9b127f9f697293cc7db2702e7926c6817f04ab
Ian Sobule <imsobule@gmail.com>
2017-05-05 14:08:05 -0500

    linked music and songlist play/pause logic

--- app.js
+++ app.js
@@ -1,4 +1,62 @@
 var model = {
-    currentSong: "",
+    nowPlaying: "",
     isPaused: true,
+
+
+};
+
+
+var songSrcs = {
+
+    springish: "music\\springish.mp3",
+    gentleman: "music\\A-Gentleman.mp3",
+    nightOwl: "music\\Night-Owl.mp3",
+    curiousCase: "music\\Curious-Case.mp3"
+
+};
+
+var songButtons = $(".song-item-group");
+
+var audio = $("<audio></audio>");
+
+$("body").append(audio);
+
+
+function PlayAudio() {
+    audio.get(0).play();
 }
+
+function PauseAudio() {
+
+    audio.get(0).pause();
+}
+
+songButtons.click(function() {
+
+    var song = $(this).attr("id");
+
+    if (song === model.nowPlaying) {
+        playPausetoggle();
+    } else {
+        model.nowPlaying = song;
+        audio.attr("src", songSrcs[song]);
+        PlayAudio();
+        model.isPaused = false;
+
+    }
+
+    function playPausetoggle() {
+
+        if (model.isPaused) {
+            PlayAudio();
+            model.isPaused = false;
+        } else {
+            console.log("im playing")
+            PauseAudio();
+            model.isPaused = true;
+        }
+    }
+
+
+
+});
