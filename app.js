850a2340b46b0afea70d5c6d28fa144be5e23e12
Ian Sobule <imsobule@gmail.com>
2017-05-07 14:22:25 -0500

    cleaned up rendering logic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Model -- things to keep track of.
 var model = {
     nowPlaying: "",
+    lastPlayed: "",
     isPaused: true,
     songs: [{
             id: 0,
@@ -29,10 +30,9 @@ var model = {
     ]
 };
 
-//Grab needed elements from DOM
-var songButtons = $(".song-item-group");
-var audio = $("<audio></audio>");
-$("body").append(audio);
+//Grab constants from DOM
+const songButtons = $(".song-item-group");
+const audio = $("#song");
 
 //Music Player Actions/Controls
 function PlayAudio() {
@@ -44,7 +44,6 @@ function PauseAudio() {
 }
 
 function playPausetoggle() {
-
     if (model.isPaused) {
         PlayAudio();
         model.isPaused = false;
@@ -57,58 +56,113 @@ function playPausetoggle() {
 // Button Event Handlers
 songButtons.click(function() {
     var selectedSongId = $(this).attr("id");
-
     var selectedSong = model.songs[selectedSongId];
 
     //Pauses and Plays selected song
     if (selectedSong === model.nowPlaying) {
         playPausetoggle();
-        renderIcons();
+        render();
 
     } else {
-        //If selected song Changes, new Song plays
+        //If selected song Changes, update model, play, and render.
+        model.lastPlayed = model.nowPlaying;
         model.nowPlaying = selectedSong;
         audio.attr("src", selectedSong.src);
         model.isPaused = false;
-        renderIcons();
+        render();
         PlayAudio();
     }
 });
 
+
 //Event Handlers for Control panel
+
 var nextButton = $("#next");
 var previousButton = $("#previous");
 var playPauseButton = $("#play-pause");
 var listLen = model.songs.length;
 
-//Next Button
+//  Next Button
 nextButton.click(function() {
     var currentIdx = model.nowPlaying.id;
     var newIdx = clamp(currentIdx + 1, listLen - 1);
     model.nowPlaying = model.songs[newIdx];
+    model.lastPlayed = model.songs[currentIdx];
     audio.attr("src", model.nowPlaying.src);
     model.isPaused = false;
-    renderIcons();
+    render();
     PlayAudio();
 });
 
-//Previous Button
+//  Previous Button
 previousButton.click(function() {
     var currentIdx = model.nowPlaying.id;
     var newIdx = clamp(currentIdx - 1, listLen - 1);
     model.nowPlaying = model.songs[newIdx];
+    model.lastPlayed = model.songs[currentIdx];
     audio.attr("src", model.nowPlaying.src);
     model.isPaused = false;
-    renderIcons();
+    render();
     PlayAudio();
 });
 
-// Play / Pause button
-
+//  Play / Pause button
 playPauseButton.click(function() {
+    if (model.nowPlaying === "") {
+        model.nowPlaying = model.songs[0];
+        audio.attr("src", model.nowPlaying.src);
+        $("#" + model.nowPlaying.id).toggleClass("active");
+    }
     playPausetoggle();
-    renderIcons();
-})
+    render();
+});
+
+// Render Function
+function render() {
+
+    //Load Now playing status in Header
+    var nowPlayingelem = $("#now-playing");
+    nowPlayingelem.text(model.nowPlaying.title + " " + model.nowPlaying.artist);
+
+    //Change Active status on elements
+
+    //Toggle Active Status on last played song
+    if (model.lastPlayed !== model.nowPlaying) {
+        $("#" + model.lastPlayed.id)
+            .removeClass("active")
+            .find(".play-button")
+            .removeClass("fa-play fa-pause")
+            .addClass("fa-play");
+    }
+
+    //Toggle active status on current song
+    var activeElem = $("#" + model.nowPlaying.id);
+    if (!activeElem.hasClass("active")) {
+        $("#" + model.nowPlaying.id)
+            .addClass("active");
+    }
+
+    //Grab active song icon and play button
+    var activeIcon = activeElem.find(".play-button");
+    var footerButton = $("#play-pause");
+
+    //Render as pause
+    if (!model.isPaused) {
+        activeIcon
+            .removeClass("fa-play")
+            .addClass("fa-pause");
+        footerButton.text("pause");
+    }
+    //Render as play
+    else {
+        activeIcon
+            .removeClass("fa-pause")
+            .addClass("fa-play");
+        footerButton.text("play");
+    }
+}
+
+// Helper Functions
 
 function clamp(num, max) {
     if (num < 0) {
@@ -118,31 +172,4 @@ function clamp(num, max) {
         return max;
     }
     return num;
-}
-
-function renderIcons() {
-
-    //render all Icons as play buttons
-    var icons = $(".play-button");
-
-    var footerButton = $("#play-pause");
-
-    icons
-        .removeClass("fa-play fa-pause")
-        .addClass("fa-play");
-
-    footerButton.text("play");
-
-    //Select active-Icon and render it as play or pause depending on state
-    var activeIcon = icons.filter(function(index) {
-        return index == model.nowPlaying.id;
-    });
-
-    if (!model.isPaused) {
-        activeIcon
-            .addClass("fa-pause");
-        footerButton.text("pause");
-    }
-
-
 }
